d6721a92b37e147e5851b30047704066c07d59e6
john Hoeven <jvdh_2@hotmail.com>
2018-12-20 07:30:30 -0800

    First change

--- DemoApp2/Program.cs
+++ DemoApp2/Program.cs
@@ -7,6 +7,7 @@ namespace DemoApp2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("those are my changes");
         }
     }
 }
