661538f9586711175bf60753b478d6d3d2912af3
john Hoeven <jvdh_2@hotmail.com>
2018-12-20 07:24:34 -0800

    First version

---
+++ DemoApp2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DemoApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
